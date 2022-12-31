0f41b73e19d4f0f8b4fa0271c5524639f320ff01
PiyushSendurse <piyushsendurse@gmail.com>
2022-12-31 16:59:33 +0530

    Account Code Added

--- Day1/Account/Program.cs
+++ Day1/Account/Program.cs
@@ -1,7 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Banking;
+using System.Collections.Generic;
 
 Account account=new Account();
 account.Deposit(2000);
-Console.WriteLine(account.GetBalance());
+Console.WriteLine("Shumshuddin credited :"+account.GetBalance());
+
+account.Withdraw(1500);
+Console.WriteLine("Shumshuddin debited :"+1500);
+Console.WriteLine("Shumshuddin current Balance :"+account.GetBalance());
+
+
+Account account1=new Account();
+account1.Deposit(6000);
+Console.WriteLine("Mainuddin credited :"+account1.GetBalance());
+
+account1.Withdraw(2000);
+Console.WriteLine("Mainuddin debited :"+2000);
+Console.WriteLine("Mainuddin current Balance :"+account1.GetBalance());
+
+//Using Collections
+List<Account> accounts=new List<Account>();
+accounts.Add(account);
+accounts.Add(account1);
+
+foreach(Account theAccount in accounts){
+    float bal=theAccount.GetBalance();
+    Console.Write(bal+" ");
+}
+
 
